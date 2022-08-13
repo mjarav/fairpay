5e7d07c753c5202411031dc0cdbdc4d358802b54
Jasan Sangma <waldura700@gmail.com>
2022-08-13 19:52:59 +0530

    validations done

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,15 @@
 class Service < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
+
+  validates :name, :description, :location, presence: true
+  validate :any_present?
+
+  def any_present?
+    if %w(monday tuesday wednesday thursday friday saturday sunday).all?{|attr| self[attr].blank?}
+      errors.add :base, "at least pick a day in a week"
+    end
+  end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :services, dependent: :destroy
+  has_many :bookings, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :chatrooms_as_customer, class_name: "Chatroom", foreign_key: :customer_id
+  has_many :chatrooms_as_provider, class_name: "Chatroom", foreign_key: :provider_id
 end

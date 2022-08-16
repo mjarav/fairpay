42f6d0e49205e21494b1975eb50207ccb41b5634
Monica Jara <jara.monica@gmail.com>
2022-08-16 23:56:24 +0200

    added checkboxes and styling

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,6 +1,8 @@
 class ServicesController < ApplicationController
   def index
+
     if params[:query].present?
+      raise
       @services = Service.global_search(params[:query])
     else
       @services = Service.all

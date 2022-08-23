966d3473437c4f46c75b3d5082015cc35603fef9
Maria Olbrich <olbrich15@gmail.com>
2022-08-23 14:00:18 +0200

    front-changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
 
-    @my_services = Service.where(user_id: current_user)
+    @my_services = Service.where(user_id: params[:id])
   end
 end

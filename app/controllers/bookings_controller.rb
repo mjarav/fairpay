1c0af8c261a631cbf313634a3dae517a236861aa
Jasan Sangma <waldura700@gmail.com>
2022-08-25 00:48:55 +0530

    conflict issues solved

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -11,6 +11,9 @@ class BookingsController < ApplicationController
     @my_booking = Booking.find(params[:id])
     @my_booking.confirmation = true
     @my_booking.save!
+    redirect_to bookings_path
+  end
+
   def show
     @booking = Booking.find(params[:id])
   end
@@ -25,6 +28,7 @@ class BookingsController < ApplicationController
   def decline
     @booking = Booking.find(params[:id])
     @booking.decline!
+
     redirect_to bookings_path
   end
 
@@ -36,17 +40,15 @@ class BookingsController < ApplicationController
     @booking = Booking.new(booking_params)
     @booking.user = current_user
     @booking.service = Service.find(params[:service_id])
-    if @booking.save
-      redirect_to bookings_path, notice: "Your booking has been created"
-    else
-      render 'new'
-    end
+    @booking.save!
+
+    redirect_to bookings_path, notice: "Your booking has been created"
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :user, :service_id, :description)
+    params.require(:booking).permit(:start_date, :end, :user, :service_id)
   end
 
   def set_service

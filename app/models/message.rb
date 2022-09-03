8367213fe6564da8f5398b6ff96450efe3383dd3
Maria Olbrich <olbrich15@gmail.com>
2022-09-03 12:18:19 +0200

    index route

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,5 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :chatroom
 
+  validates :content, presence: true, allow_blank: false
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
       post :decline
     end
   end
-  resources :chatrooms, only: [:show, :create] do
+  resources :chatrooms, only: [:index, :show, :create] do
     resources :messages, only: [:create]
   end
 end

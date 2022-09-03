800729ccb29f70e4674355a64f9eba96758c103d
Monica Jara <jara.monica@gmail.com>
2022-09-03 11:46:33 +0200

    added reviewed status

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,7 +5,7 @@ class Booking < ApplicationRecord
   validates :start_date, :description, presence: true, on: :create
   validate :start_date_after_today
 
-  enum status: [:approve, :declined, :accepted, :completed]
+  enum status: [:approve, :declined, :accepted, :completed, :reviewed]
 
   def format_status_for(user_type)
     return "Waiting" if approve? && user_type == :customer
@@ -25,7 +25,9 @@ class Booking < ApplicationRecord
     update(status: :completed)
   end
 
-
+  def review!
+    update(status: :reviewed)
+  end
 
   private
 
@@ -33,7 +35,7 @@ class Booking < ApplicationRecord
 
     return if start_date.blank?
 
-    if start_date < Date.today && status != "completed" && status != "declined"
+    if start_date < Date.today && status != "completed" && status != "declined"  && status != "reviewed"
       errors.add(:start_date, "must be after today")
     end
   end

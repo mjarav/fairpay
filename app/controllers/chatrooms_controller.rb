da3bc3f7fe54c72f3534f2b6a813804ef3c03176
Maria Olbrich <olbrich15@gmail.com>
2022-09-03 14:42:37 +0200

    chat working

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,15 +1,28 @@
 class ChatroomsController < ApplicationController
+  def index
+    @chatrooms = Chatroom.where(provider: current_user).or(Chatroom.where(customer: current_user))
+    @chatrooms = @chatrooms.sort { |a, b| (b.messages.last['created_at']) <=> (a.messages.last['created_at']) }
+  end
+
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
   end
 
   def create
-    @chatroom = Chatroom.new
-    @chatroom.customer = current_user
-    @chatroom.provider = User.find(params[:provider])
-    @chatroom.save
+    name = [current_user.id, User.find(params[:provider]).id].sort
+    @chatroom = Chatroom.where(name: "#{name[0]}-#{name[1]}")
 
-    redirect_to chatroom_path(@chatroom)
+    if @chatroom.length.positive?
+      redirect_to chatroom_path(@chatroom[0])
+    else
+      @chatroom = Chatroom.new
+      @chatroom.customer = current_user
+      @chatroom.provider = User.find(params[:provider])
+      @chatroom.name = "#{name[0]}-#{name[1]}"
+      @chatroom.save
+
+      redirect_to chatroom_path(@chatroom)
+    end
   end
 end
